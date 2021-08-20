a9677cc72e1f0119f77f4f2f1d07ce442cd793cd
emorantesEdutecno <emorantes@edutecno.com>
2021-08-20 15:43:52 -0500

    primer commit

---
+++ assets/js/modules/saludo.js
@@ -0,0 +1,12 @@
+export default {
+   saludoDia: ()=>{
+     console.log('Buenos dias');
+   },
+   saludoTarde: ()=>{
+     console.log('Buenas tardes');
+   },
+   saludoNoche: ()=>{
+     console.log('Buenas noches'):
+   },
+
+}// fin export
---
+++ assets/js/scripts.js
@@ -0,0 +1,3 @@
+import saludar from './modules/saludo.js';
+
+saludar.saludoNoche;
