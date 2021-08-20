b7de4e3fa7937f43f435b3fc01ed463681b7ffb4
emorantesEdutecno <emorantes@edutecno.com>
2021-08-20 16:06:10 -0500

    boton restar

--- assets/js/modules/cargaDatos.js
+++ assets/js/modules/cargaDatos.js
@@ -2,9 +2,28 @@ export default {
   datosBotonSumar: ()=>{
     let elBotonSumar = document.getElementById('botonSumar');
     elBotonSumar.addEventListener('click', realizarSuma);
+
+  },
+  datosBotonRestar: ()=>{
+    let elBotonRestar = document.getElementById('botonRestar');
+    elBotonRestar.addEventListener('click', realizarResta);
   }
 }//Fin export
 
 let realizarSuma = ()=>{
   console.log('Funciona el boton Suma');
+  let dato1 = 4;
+  let dato2 = 3;
+  let resultado = dato1+dato2;
+  let mensaje = `El resultado de sumar ${dato1} con ${dato2} es: ${resultado}`;
+  console.log(mensaje);
+}
+
+let realizarResta = ()=>{
+  console.log('Funciona el boton Resta');
+  let dato1 = 4;
+  let dato2 = 3;
+  let resultado = dato1-dato2;
+  let mensaje = `El resultado de restar ${dato1} con ${dato2} es: ${resultado}`;
+  console.log(mensaje);
 }
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,4 +2,5 @@ import cargar from './modules/cargaDatos.js';
 import saludar from './modules/saludo.js';
 
 cargar.datosBotonSumar();
+cargar.datosBotonRestar();
 saludar.saludoNoche();
